6453348d0e7d05d576ebe7cf8c8f2efd64262347
lucy <lucy@luz.lu>
2014-04-11 01:22:16 +0200

    rewrite size.go

--- size.go
+++ size.go
@@ -1,35 +1,49 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+func size(w writer, size int64) {
+	const (
+		nnull  = "0123456789"
+		nblank = " 123456789"
+	)
 
-// IEC Sizes; kibis of bits
-const (
-	Byte = 1
-	KiB  = Byte * 1024
-	MiB  = KiB * 1024
-	GiB  = MiB * 1024
-	TiB  = GiB * 1024
-	PiB  = TiB * 1024
-	EiB  = PiB * 1024
-)
+	var (
+		idx int
+		dec int64
+		buf [3]byte
+		fmt string
+	)
 
-// Human readable bytes
-func size(b writer, s int64) {
-	const base = 1024
-	if s < 10 {
-		fmt.Fprintf(b, "%s%3d%s", cSize, s, cSizes[0])
-		return
+	for size > 999 {
+		if size > 1024 {
+			idx++
+		}
+		dec = size
+		size /= 1024
 	}
-	f := float64(s)
-	e := math.Floor(math.Log(f) / math.Log(base))
-	suffix := cSizes[int(e)]
-	val := f / math.Pow(base, e)
-	if val >= 10 {
-		fmt.Fprintf(b, "%s%3v%s", cSize, int(val), suffix)
+	// round <1.05 && >=0.95 to 1
+	if dec < 105*1024/100 && dec >= 95*1024/100 {
+		size = 1
+		idx++
+	}
+	dec = (dec*1000/1024 + 50) / 100 % 10
+
+	fmt = nblank
+	if size >= 10 {
+		// value is >= 10: use "123M', " 12M" formats
+		buf[0] = fmt[size/100]
+		if size/100 != 0 {
+			fmt = nnull
+		}
+		buf[1] = fmt[size/10%10]
+		buf[2] = byte('0' + size%10)
 	} else {
-		fmt.Fprintf(b, "%s%3v%s", cSize, math.Floor(val*10)/10, suffix)
+		// value is < 10: use "9.2M" format
+		buf[0] = byte('0' + size)
+		buf[1] = '.'
+		buf[2] = byte('0' + dec)
 	}
+
+	w.Write(cSize)
+	w.Write(buf[:])
+	w.Write(cSizes[idx])
 }

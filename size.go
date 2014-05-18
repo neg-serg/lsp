34ddf2bbcdf88ddb4ff899c56b237a4f9bb08249
lucy <lucy@luz.lu>
2014-05-18 20:38:29 +0200

    size: fix rounding again

--- size.go
+++ size.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func size(w writer, size int64) {
 	s := float64(size)
@@ -12,7 +10,7 @@ func size(w writer, size int64) {
 		s /= 1024
 	}
 	w.Write(cSize)
-	if s < 10.0 && s != 0 {
+	if s != 0 && s < 9.95 {
 		fmt.Fprintf(w, "%3.1f", s)
 	} else {
 		fmt.Fprintf(w, "%3.0f", s)
